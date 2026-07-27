704739d073789469fbf7937df72a91993194e763
Domenico Pagano <dpaganoh@gmail.com>
2026-07-27 15:58:32 -0500

    Terminado foundations 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || start < 0 || !Number.isInteger(end) || end < 0) return "ERROR";
+    if(start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
